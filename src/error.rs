ee45ecd1f98c7e916c14d32a0d4569539dd587a9
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-08-17 11:51:45 +0000

    Add basic documentation

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,16 @@
 
 use failure::Fail;
 
+/// Result wrapper
 pub type Result<T> = std::result::Result<T, failure::Error>;
 
+/// Error types
 #[derive(Debug, Fail)]
 pub enum Error {
+    /// Serialization error
     #[fail(display = "Serialization error: {}", _0)]
     HashSerialize(String),
+    /// None error
     #[fail(display = "Option.None an error")]
     NoneError,
 }
